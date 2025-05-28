1e3ebadf635e30434d0e4dab07d060e7ed9efb17
gubert-duda <160794352+gubert-duda@users.noreply.github.com>
2025-05-28 19:02:29 -0300

    Update main.js

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-07-20T23:59:00");
 const tempoObjetivo2 = new Date("2025-06-13T23:59:00");
 const tempoObjetivo3 = new Date("2026-01-20T23:59:00");
-const tempoObjetivo4 = new Date("2025-12-07T29:59:00");
+const tempoObjetivo4 = new Date("2025-12-07T13:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
